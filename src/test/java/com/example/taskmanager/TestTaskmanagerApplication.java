ee9b118670f1cee1c1d194335e7d2fbc4b02725b
Prashoon Jha <prashoon24@gmail.com>
2025-11-21 16:23:39 +0200

    added app.yml and app prod.yml

---
+++ src/test/java/com/example/taskmanager/TestTaskmanagerApplication.java
@@ -0,0 +1,11 @@
+package com.example.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TaskmanagerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
